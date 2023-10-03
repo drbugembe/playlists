251c0129f95c604c3cf5ca85c384b700c9ca9d36
bugembejohnpaul <bugembejohnpaul@gmail.com>
2023-10-03 11:47:25 +0300

    Allowing Hosts

--- youtubeplaylist/settings.py
+++ youtubeplaylist/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-splf2$eyos6w&!xntd-zf!qggk_1j*b@regkhj-^^6udferieb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
